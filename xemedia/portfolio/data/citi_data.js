3192d7c3e60747bb3f262043715457607d7fcb53
Chris Mathers <xee@xemedia.com>
2016-10-20 21:46:18 +0100

    Move wordpress up a level

--- xemedia/portfolio/data/citi_data.js
+++
@@ -1,5 +0,0 @@
-[{
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='images/citi/citi_corpActions_782x475.png' alt='Citi'></a></div>"
-}, {
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='images/citi/citi_corpActionsSummary_782x475.png' alt='Citi'></a></div>"
-}]
--- xemedia/portfolio/data/prudential_data.js
+++
@@ -1,5 +0,0 @@
-[{
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='images/prudential/Prudential_splash_782x475.png' alt='Prudential'></a></div>"
-}, {
-      "content": "<div class='slide_inner'><a class='photo_link' href='#'><img class='photo' src='images/prudential/Prudential_overlay_782x475.png' alt='Prudential'></a></div>"
-}]
